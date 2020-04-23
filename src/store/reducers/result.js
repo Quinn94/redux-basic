dce918c8c7840c9c9a47dce6b09823510c8ea990
Quinn94 <mayquinn94@gmail.com>
2020-04-23 08:53:41 +0800

    redux-thunk

--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -5,14 +5,18 @@ const initialState = {
     results: []
 }
 
+const deleteResults = (state, action) => {
+    const updatedArray = state.results.filter(results => results.id !== action.resultElId);
+    return updateObject(state, {result: updatedArray})
+}
+
 const resultReducer = (state=initialState, action) => {
     switch(action.type){
         case actionTypes.STORE_RESULTS:
             return updateObject(state, {results: state.results.concat({id: new Date(), value: action.res})});
 
         case actionTypes.DELETE_RESULTS:
-            const updatedArray = state.results.filter(results => results.id !== action.resultElId);
-            return updateObject(state, {result: updatedArray});
+            return deleteResults(state, action);
     }
     return state;
 }
